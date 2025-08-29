75c9a1e6c6fa5bea4c0ead5d169dfcba4890ae1f
Diane Kim <diane.kim@rcl-nv1.ece.ubc.ca>
2025-08-29 01:56:57 -0700

    set up for tab+video experiments

--- baseline/cross_attn/src/eval_helper.py
+++ baseline/cross_attn/src/eval_helper.py
@@ -31,32 +31,102 @@ def compute_worst_class_accuracy(y_true, y_pred):
     return worst_acc
 
 
-def show_confusion_matrix(y_true, y_pred, set_name, run=DummyRun(), auto_close=True):
-    conf_matrix = confusion_matrix(y_true, y_pred)
-    print("\nConfusion Matrix:\n", conf_matrix)
+# def show_confusion_matrix(y_true, y_pred, set_name, run=DummyRun(), auto_close=True):
+#     conf_matrix = confusion_matrix(y_true, y_pred)
+#     print("\nConfusion Matrix:\n", conf_matrix)
 
-    fig = plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix,
-                annot=True,
-                fmt='d',
-                cmap="Blues",
-                # xticklabels=["MANAG", "PTCA", "SURG"],
-                # yticklabels=["MANAG", "PTCA", "SURG"]
-                xticklabels=["MANAG", "INTERVENTION"],
-                yticklabels=["MANAG", "INTERVENTION"]
-                )
-    plt.ylabel('Actual Class')
-    plt.xlabel('Predicted Class')
-    plt.title('Confusion Matrix')
-    if not isinstance(run, DummyRun):
-        run.log({f'confusion_matrix/{set_name}': wandb.Image(fig)}, commit=False)
+#     fig = plt.figure(figsize=(8, 6))
+#     sns.heatmap(conf_matrix,
+#                 annot=True,
+#                 fmt='d',
+#                 cmap="Blues",
+#                 # xticklabels=["MANAG", "PTCA", "SURG"],
+#                 # yticklabels=["MANAG", "PTCA", "SURG"]
+#                 xticklabels=["MANAG", "INTERVENTION"],
+#                 yticklabels=["MANAG", "INTERVENTION"]
+#                 )
+#     plt.ylabel('Actual Class')
+#     plt.xlabel('Predicted Class')
+#     plt.title('Confusion Matrix')
+#     if not isinstance(run, DummyRun):
+#         run.log({f'confusion_matrix/{set_name}': wandb.Image(fig)}, commit=False)
 
-    if auto_close:
-        plt.close()
+#     if auto_close:
+#         plt.close()
 
+# def class_accuracy(y_true, y_pred):
+#     classes = np.unique(y_true)
+#     out = {}
+#     for c in classes:
+#         mask = (y_true == c)
+#         denom = mask.sum()
+#         out[int(c) if hasattr(c, "__init__") else c] = (float((y_pred[mask] == c).sum()/denom) if denom else np.nan)
+#     return out
 
-def safe_auc_score(y_true, y_prob, average='macro', multi_class='ovr'):
-    try:
-        return 100.0 * roc_auc_score(y_true, y_prob, average=average, multi_class=multi_class)
-    except ValueError:
-        return float('nan')  # e.g. if only one class present
+# def _sens_at_spec(y_true, y_scores, targets=(0.2, 0.4, 0.6)):
+#     """Return dict: spec_target -> (sensitivity, specificity, threshold). Ex. {0.2: (sens, spec, thr), 0.4: (...), 0.6: (...)}"""
+#     fpr, tpr, thr = roc_curve(y_true, y_scores)
+#     spec = 1.0 - fpr
+#     out = {}
+#     for t in targets:
+#         idxs = np.where(spec>=t)[0] #unwraps the tuple array
+#         if len(idxs) == 0:
+#             out[t] = (np.nan, np.nan, np.nan)
+#         else:
+#             i = idxs[-1]
+#             out[t] = (tpr[i], spec[i], thr[i])
+#     return out
+
+# def model_eval(y_true, y_pred, y_proba, positive_class=1, specificities=(0.2, 0.4, 0.6), 
+#          average="weighted", verbose=True, set_name=None):
+    
+#     y_true = np.asarray(y_true)
+    
+#     scores = None
+
+#     y_proba=np.asarray(y_proba)
+#     if y_proba.ndim == 2:
+#         scores = y_proba[:, 1]
+#     else:
+#         scores = y_proba.ravel()
+    
+#     y_pred = np.asarray(y_pred)
+
+#     metrics = {
+#         "ROC AUC": (roc_auc_score(y_true, scores) if scores is not None else np.nan),
+#         "Accuracy": accuracy_score(y_true, y_pred),
+#         "Balanced Accuracy": balanced_accuracy_score(y_true, y_pred),
+#         "F1 Score": f1_score(y_true, y_pred, average=average, zero_division=0),
+#         "Recall": recall_score(y_true, y_pred, average=average, zero_division=0),
+#         "Precision": precision_score(y_true, y_pred, average=average, zero_division=0),
+#         "Class Accuracy": class_accuracy(y_true, y_pred),
+#     }
+
+#     sens_at_spec = {}
+#     if scores is not None and len(np.unique(y_true)) == 2:
+#         sens_at_spec = _sens_at_spec(y_true, scores, specificities)
+    
+#     result = {
+#         "metrics": metrics,
+#         "sensitivities": sens_at_spec
+#     }
+
+#     if verbose:
+#         head = []
+#         if set_name: head.append(set_name.upper())
+#         # if epoch is not None: head.append(f"Epoch {epoch}")
+#         if head: print("[" + " | ".join(head) + "]")
+#         for k, v in metrics.items():
+#             if k == "Class Accuracy":
+#                 print("Class Accuracy:")
+#                 for cls, acc in v.items():
+#                     print (f"  {cls}: {acc:.4f}" if acc == acc else f"  {cls}: nan")
+#             else: 
+#                 print(f"{k}: {v:.4f}" if isinstance(v, (float, np.floating)) else f"{k}: {v}")
+#         if sens_at_spec:
+#             for t, (sens, spec, thr) in sens_at_spec.items():
+#                 pct = int(t*100)
+#                 print(f"Spec {pct}% → Sens: {sens:.4f}, Spec: {spec:.4f}, Thr: {thr:.4f}")
+#         print()
+
+#     return result
